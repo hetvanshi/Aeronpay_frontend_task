0e8221248011204df61a4acb3a9dada2a6e19d6f
hetvanshi <hetvanshigohel1998@gmail.com>
2025-11-25 14:27:42 +0530

    Added static export config and out folder

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: "export",    
+  distDir: "out",      
 };
 
 export default nextConfig;
